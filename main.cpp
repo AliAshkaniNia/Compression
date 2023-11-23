9ce9cfbe4303ca9bef60ca43ba1f09d244e31272
Ali Ashkani Nia <ashkaninia@gmail.com>
2023-11-23 03:50:06 +0330

    fixed an issue in output

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char* argv[]) {
         compression->decode(inputText, outputText);
     }
 
-    if(compression->saveToFile(inputFileName, inputText) != 0){
+    if(compression->saveToFile(outputFileName, outputText) != 0){
         return 1;
     }
 
